d2d2bfe4de039e885d9039bb6a5947f3d3dd8b7c
hitman <hitmanvi@qq.com>
2025-11-03 09:49:01 +0800

    update

--- app/Http/Controllers/UtilsController.php
+++ app/Http/Controllers/UtilsController.php
@@ -18,6 +18,8 @@ class UtilsController extends Controller
     {
         return $this->responseItem([
             'supported_locales' => ['en', 'zh-CN', 'ja', 'ko'],
+            'app_limit' => config('app.app_limit', 10),
+            'web_limit' => config('app.web_limit', 10),
         ]);
     }
 }
