7f2e3f9e3d63c36a3466de40a1d792ba51ba52a4
hitman <hitmanvi@qq.com>
2025-11-05 14:08:21 +0800

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Exceptions\Exception;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         
         $exceptions->render(function (\Exception $e) {
+            Log::error($e->getMessage());
             $resp = [
                 'code'   => ErrorCode::INTERNAL_ERROR->value,
                 'errmsg' => "SERVER ERROR",
