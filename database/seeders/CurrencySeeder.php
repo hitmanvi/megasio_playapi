5fb99efac4fbd9084774841aa2de8c8ce61f02ac
hitman <hitmanvi@qq.com>
2025-11-24 15:26:32 +0800

    update

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -14,17 +14,17 @@ class CurrencySeeder extends Seeder
     {
         $currencies = [
             // 法币
-            ['code' => 'USD', 'name' => 'US Dollar', 'type' => Currency::TYPE_FIAT, 'symbol' => '$', 'icon' => 'usd.svg', 'sort_id' => 1],
-            ['code' => 'EUR', 'name' => 'Euro', 'type' => Currency::TYPE_FIAT, 'symbol' => '€', 'icon' => 'eur.svg', 'sort_id' => 2],
-            ['code' => 'CNY', 'name' => 'Chinese Yuan', 'type' => Currency::TYPE_FIAT, 'symbol' => '¥', 'icon' => 'cny.svg', 'sort_id' => 3],
-            ['code' => 'JPY', 'name' => 'Japanese Yen', 'type' => Currency::TYPE_FIAT, 'symbol' => '¥', 'icon' => 'jpy.svg', 'sort_id' => 4],
-            ['code' => 'KRW', 'name' => 'South Korean Won', 'type' => Currency::TYPE_FIAT, 'symbol' => '₩', 'icon' => 'krw.svg', 'sort_id' => 5],
-            ['code' => 'GBP', 'name' => 'British Pound', 'type' => Currency::TYPE_FIAT, 'symbol' => '£', 'icon' => 'gbp.svg', 'sort_id' => 6],
+            ['code' => 'USD', 'name' => 'US Dollar', 'type' => Currency::TYPE_FIAT, 'symbol' => '$', 'icon' => 'https://s2.coinmarketcap.com/static/cloud/img/fiat-flags/USD.svg', 'sort_id' => 1],
+            ['code' => 'EUR', 'name' => 'Euro', 'type' => Currency::TYPE_FIAT, 'symbol' => '€', 'icon' => 'https://s2.coinmarketcap.com/static/cloud/img/fiat-flags/EUR.svg', 'sort_id' => 2],
+            ['code' => 'CNY', 'name' => 'Chinese Yuan', 'type' => Currency::TYPE_FIAT, 'symbol' => '¥', 'icon' => 'https://s2.coinmarketcap.com/static/cloud/img/fiat-flags/CNY.svg', 'sort_id' => 3],
+            ['code' => 'JPY', 'name' => 'Japanese Yen', 'type' => Currency::TYPE_FIAT, 'symbol' => '¥', 'icon' => 'https://s2.coinmarketcap.com/static/cloud/img/fiat-flags/JPY.svg', 'sort_id' => 4],
+            ['code' => 'KRW', 'name' => 'South Korean Won', 'type' => Currency::TYPE_FIAT, 'symbol' => '₩', 'icon' => 'https://s2.coinmarketcap.com/static/cloud/img/fiat-flags/KRW.svg', 'sort_id' => 5],
+            ['code' => 'GBP', 'name' => 'British Pound', 'type' => Currency::TYPE_FIAT, 'symbol' => '£', 'icon' => 'https://s2.coinmarketcap.com/static/cloud/img/fiat-flags/GBP.svg', 'sort_id' => 6],
             // 加密货币
-            ['code' => 'BTC', 'name' => 'Bitcoin', 'type' => Currency::TYPE_CRYPTO, 'symbol' => '₿', 'icon' => 'btc.svg', 'sort_id' => 10],
-            ['code' => 'ETH', 'name' => 'Ethereum', 'type' => Currency::TYPE_CRYPTO, 'symbol' => 'Ξ', 'icon' => 'eth.svg', 'sort_id' => 11],
+            ['code' => 'BTC', 'name' => 'Bitcoin', 'type' => Currency::TYPE_CRYPTO, 'symbol' => '₿', 'icon' => 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png', 'sort_id' => 10],
+            ['code' => 'ETH', 'name' => 'Ethereum', 'type' => Currency::TYPE_CRYPTO, 'symbol' => 'Ξ', 'icon' => 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png', 'sort_id' => 11],
             ['code' => 'USDT', 'name' => 'Tether', 'type' => Currency::TYPE_CRYPTO, 'symbol' => '₮', 'icon' => 'usdt.svg', 'sort_id' => 12],
-            ['code' => 'BNB', 'name' => 'BNB', 'type' => Currency::TYPE_CRYPTO, 'symbol' => 'BNB', 'icon' => 'bnb.svg', 'sort_id' => 13],
+            ['code' => 'BNB', 'name' => 'BNB', 'type' => Currency::TYPE_CRYPTO, 'symbol' => 'BNB', 'icon' => 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png', 'sort_id' => 13],
             ['code' => 'SOL', 'name' => 'Solana', 'type' => Currency::TYPE_CRYPTO, 'symbol' => '◎', 'icon' => 'sol.svg', 'sort_id' => 14],
         ];
 
