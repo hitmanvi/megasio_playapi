ee55ba7259ce4c25bb5e1a11a31fe6fafb171a6e
hitman <hitmanvi@qq.com>
2025-12-08 15:31:36 +0800

    update

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -100,7 +100,7 @@ class DepositController extends Controller
             $request->input('deposit_info', []),
             $request->input('extra_info', []),
             $request->ip(),
-            $request->input('native_app', '')
+            $request->input('native_app', 0),
         );
 
         // 返回创建的存款订单信息
