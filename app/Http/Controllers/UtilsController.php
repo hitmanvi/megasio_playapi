7ae57247845aa61d11c8dbb0518f9984566cd9a6
hitman <hitmanvi@qq.com>
2026-03-05 11:52:52 +0800

    update

--- app/Http/Controllers/UtilsController.php
+++ app/Http/Controllers/UtilsController.php
@@ -19,14 +19,6 @@ class UtilsController extends Controller
     }
 
     /**
-     * 测试接口：展示 getDeviceInfo 获取到的设备信息
-     */
-    public function deviceInfo(Request $request)
-    {
-        return $this->responseItem($this->getDeviceInfo($request));
-    }
-
-    /**
      * 获取应用设置
      */
     public function settings()
--- routes/api.php
+++ routes/api.php
@@ -221,7 +221,6 @@ Route::prefix('help-center')->group(function () {
 
 // 工具类路由
 Route::get('/timestamp', [UtilsController::class, 'timestamp']);
-Route::get('/device-info', [UtilsController::class, 'deviceInfo']);
 Route::get('/settings', [UtilsController::class, 'settings']);
 Route::middleware('auth:sanctum')->post('/upload', [UtilsController::class, 'uploadImage']);
 
